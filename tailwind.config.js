0d60feb02ac69f108b615e0c1c8df9f7acdddde1
Mchrispin <mchrispin14@gmail.com>
2024-09-10 08:26:57 +0200

    'added-menu-button-animations'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
-/** @type {import('tailwindcss').config} */
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./build/*.html'],
+    content: ["./build/*.html", "./build/js/*.js"],
     theme: {
-        extend: {
-            screens: {
-                'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
-                'tailscreen': {'raw': '(min-aspect-ratio: 1/2)'},
-            }
+      extend: {
+        screens: {
+          'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+          'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
         },
+        keyframes: {
+          'open-menu': {
+            '0%': { transform: 'scaleY(0)' },
+            '80%': { transform: 'scaleY(1.2)' },
+            '100%': { transform: 'scaleY(1)' },
+          },
+        },
+        animation: {
+          'open-menu': 'open-menu 0.5s ease-in-out forwards',
+        },
+      },
     },
     plugin: [],
-}
+}  
